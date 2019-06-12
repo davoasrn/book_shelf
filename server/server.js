68b2efbf1820765c2c01ad50b08de512c2a459d0
Davit Asryan <davoasrn@gmail.com>
2019-06-11 20:40:29 -0700

    server configuration changes

--- server/server.js
+++ server/server.js
@@ -15,7 +15,9 @@ mongoose.connect(config.DATABASE,{
 });
 
 app.use(bodyParser.json());
-app.use(cookieParser())
+app.use(cookieParser());
+
+app.use(express.static('client/build'));
 
 
 //GET
@@ -56,6 +58,13 @@ app.put('/api/user', user.update)
 //DELETE
 app.delete('/api/delete_book', book.delete);
 app.delete('/api/user', user.delete);
+
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build','index.html' ))
+    })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
